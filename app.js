0b613bc0f5ade92af92d138384c25818ecb1227e
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2022-06-27 11:22:09 -0400

    fix() grapqh server

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 const { typeDefs } = require('./typeDefs')
 const { resolvers } = require('./resolvers')
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 require('dotenv').config({ path: __dirname + '/.env' })
 
 const { connectDB } = require('./db')
 connectDB()
 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
 const app = express()
 
 app.get('/', (req, res) => res.send('Welcome to API'))
 // app.use('*', (req, res) => res.status(404).send('Not found'))
-
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 
 async function start() {
@@ -22,7 +43,7 @@ async function start() {
     await apolloServer.start()
     apolloServer.applyMiddleware({ app });
 
-    port = process.env.PORT || 3000
+    port = process.env.PORT || 4000
 
     app.listen(port, () => {
         console.log(`Server on port ${port}`);
